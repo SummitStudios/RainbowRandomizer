5d020b30afef8cd14a5ed42194b0b9141c20fef2
Danny <danielvcameron@gmail.com>
2018-05-18 16:59:30 -0500

    Added borders on buttons and labels. Added our logo.

--- RainbowSixApp/ViewController.swift
+++ RainbowSixApp/ViewController.swift
@@ -12,9 +12,33 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var operatorView: UIImageView!
     
+    @IBOutlet weak var attacklabel: UILabel!
+    
+    @IBOutlet weak var defenselabel: UILabel!
+    
+    @IBOutlet weak var attackbuttonborder: UIButton!
+    
+    @IBOutlet weak var defensebuttonborder: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // attack label border
+        attacklabel.layer.borderWidth = 2.0
+        attacklabel.layer.borderColor = UIColor.white.cgColor
+        
+        // defense label border
+        defenselabel.layer.borderWidth = 2.0
+        defenselabel.layer.borderColor = UIColor.white.cgColor
+        
+        // attack button border
+        attackbuttonborder.layer.borderWidth = 2.0
+        attackbuttonborder.layer.borderColor = UIColor.white.cgColor
+        
+        // defenese button border
+        defensebuttonborder.layer.borderWidth = 2.0
+        defensebuttonborder.layer.borderColor = UIColor.white.cgColor
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,6 +63,7 @@ class ViewController: UIViewController {
         // Update Op Image
         operatorView.image = UIImage(named: "op\(randomOp)")
     }
+    
     
 
 }
